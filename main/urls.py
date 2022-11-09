7fc560c2efc83b92c62c4779e36743ed06689b8b
Andje23 <andje23@gmail.com>
2022-11-09 22:44:22 +0300

    added url on home, blog_detail, user_profile

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from main import views
+
+
+urlpatterns = [
+    path('', views.blog_home, name="home"),
+    path('blog_detail/', views.blog_detail, name="blog_detail"),
+    path('profile/', views.profile, name="user_profile"),
+
+]
