f50e5caca0bee1758d3729825897ae67a20f05f4
Andje23 <andje23@gmail.com>
2023-01-08 22:55:30 +0300

    Creating custom user registration form.

---
+++ authors/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SingnupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите свое имя'
+        })
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите свое имя'
+        })
+        self.fields['last_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите вашу фамилию'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите вашу почту'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите ваш пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Повторите ваш пароль'
+        })
+    username = forms.CharField(max_length=150)
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+    email = forms.CharField(max_length=150)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
--- authors/views.py
+++ authors/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SingnupForm
 
 
 def singUp(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SingnupForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Ваш акаунт создан успешно.")
@@ -13,7 +14,7 @@ def singUp(request):
         else:
             messages.error(request, "Error")
     else:
-        form = UserCreationForm()
+        form = SingnupForm()
     return render(request, "authors/register.html", {'form': form})
 
 
