79c78f1a0754c079b98d1c656058b76c47e78aa3
Andje23 <andje23@gmail.com>
2023-01-08 13:23:46 +0300

    Widgets in forms.

--- main/forms.py
+++ main/forms.py
@@ -8,3 +8,17 @@ class ContactForm(forms.ModelForm):
         fields = "__all__"
         # fields = ("first_name", "last_name", "e_mail")
         # exclude = ("first_name", )
+
+        widgets = {
+            "first_name": forms.TextInput(attrs={'class': 'form-control',
+                                                 'placeholder': 'Введите свое имя'}),
+            "last_name": forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите вашу фамилию'}),
+            "e-mail": forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Введите вашу почту'}),
+            "phone_number": forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваш номер телефона'}),
+            "contact_message": forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите ваше сообщение'}),
+        }
+
+
+
+
+
